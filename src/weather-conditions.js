7c2fada815bdc8ff8209c15fde466496ae139f85
Polad Mirzayev <polad.mirzayev@gmail.com>
2018-02-28 17:01:08 -0700

    Adding weather conditions dictionary

---
+++ src/weather-conditions.js
@@ -0,0 +1,12 @@
+module.exports = {
+  1: 'Clear and sunny',
+  2: 'Overcast, cloudy but no precipitation',
+  3: 'Raining',
+  4: 'Snowing, not including drifting snow',
+  5: 'Freezing rain, sleet, hail',
+  6: 'Visibility limitation',
+  7: 'Strong wind',
+  Q: 'Other',
+  U: 'Unknown',
+  X: 'Not Provided'
+}
